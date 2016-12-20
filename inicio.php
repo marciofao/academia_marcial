c3cce1c53b10395568b9d0d25b921b694971ae96
Márcio Lopes Fão <marcio.lopes.fao@gmail.com>
2016-12-20 11:35:31 -0200

    small fix

--- inicio.php
+++ inicio.php
@@ -8,6 +8,7 @@ require_once 'php_assets/header.php';
 $datas=$database->select('curriculo', ["cod_curriculo","nome_faixa", "graduacao_cod"], ["ORDER" => "graduacao_cod"]);
 
 //die(var_dump($datas));
+//die($_SESSION['graduacao_cod']);
 ?>
 
 
@@ -22,12 +23,12 @@ $datas=$database->select('curriculo', ["cod_curriculo","nome_faixa", "graduacao_
 			<a href="ver_curriculo.php?c=<?php echo $value['cod_curriculo'] ?>" class="btn-primary btn-md form-control"><?php echo $value['nome_faixa'] ?></a>
 		</div>
 		<?php 
-	}
+	}		
 	endforeach ?>
 
 	
 
 
-	
+
 
 	<?php 	require_once "php_assets/footer.php"; ?>
